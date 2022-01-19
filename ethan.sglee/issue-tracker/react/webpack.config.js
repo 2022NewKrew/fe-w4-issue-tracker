bce43baaa8b36202fe362cf24ea60917a51d3b09
ethan.sglee <ethan.sglee@kakaocorp.com>
2022-01-20 02:16:37 +0900

    Initial commit

---
+++ ethan.sglee/issue-tracker/react/webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'), // 결과물 경로
+    filename: 'bundle.js', // 결과물 파일명
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 5500,
+    client: {
+      overlay: true,
+    },
+    hot: true,
+    devMiddleware: {
+      writeToDisk: true,
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: '/node_modules/',
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        // use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(jpeg|jpg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new MiniCssExtractPlugin({ 
+      filename: 'app.css' 
+    }),
+  ],
+};

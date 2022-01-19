f17801a6d03ed43194fdf97f7f41df8018075a6f
miro-ring <miro.ring@kakaocorp.com>
2022-01-20 00:09:01 +0900

    feat: 웹팩 설정 추가

---
+++ webpack.common.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            inject: true,
+            template: './public/index.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /[\\/]node_modules[\\/]/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+};

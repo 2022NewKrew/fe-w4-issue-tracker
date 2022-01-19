6d6fd2082e7f68c1a71c5c1c091b0d1c878f488b
terajh <terajoohyun@ajou.ac.kr>
2022-01-19 18:15:33 +0900

    refactor: webpack dev, prod 분기 + 한 폴더로 관리

--- client/config/webpack.common.js
+++ client/config/webpack.common.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   entry: path.resolve(__dirname, "../src/index.tsx"),
   output: {
-    path: path.resolve(__dirname, "../dist"),
+    path: path.resolve(__dirname, "../../server/dist"),
     filename: "[name].[chunkhash].js",
     clean: true,
   },

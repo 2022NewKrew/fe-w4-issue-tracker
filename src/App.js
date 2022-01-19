04ee562e14a4333035f8566185719c964ca58c34
DONGWOOK KIM <62870938+dwkim-97@users.noreply.github.com>
2022-01-19 18:23:50 +0900

    프로젝트 환경설정

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const Box = styled.div`
+  width: 100px;
+  height: 100px; 
+  background-color: yellowgreen;
+`
+
+function App() {
+    return (
+        <div>
+            <Box>App</Box>
+        </div>
+    )
+}
+
+export default App

00d19d44becf9dab86f2ab85e212319f1e45034b
ethan-3160 <ethan.3160@kakaocorp.com>
2022-01-20 09:29:32 +0900

    Chore : express server setup

--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -1,6 +1,12 @@
-const express = require("express");
-const app = express();
+import express from "express";
+import cors from "cors";
+
 const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");

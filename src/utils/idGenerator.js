bd8c0e1ca4d32c3e8a99bd1655c61271ff226f76
brad.ley <brad.ley@kakaocorp.com>
2022-01-20 15:12:28 +0900

    fix problem to appear decimal point in id

--- src/utils/idGenerator.js
+++ src/utils/idGenerator.js
@@ -2,9 +2,9 @@
  * 랜덤하게 생성된 16진수 문자열을 생성
  * @return {string} 생성된 id
  */
-function generateId() {
-  const leftRanNum = Math.random() * 10000000000000000
-  const rightRanNum = Math.random() * 10000000000000000
+export function generateId() {
+  const leftRanNum = Math.floor(Math.random() * 1000000000000000)
+  const rightRanNum = Math.floor(Math.random() * 10000000000000000)
   
   return leftRanNum.toString(16) + rightRanNum.toString(16)
 }

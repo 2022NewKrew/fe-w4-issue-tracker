90f11e9a11219c69c78cd153de32716f24cc1a2d
Chae Lin Shin <chaelinshin96@gmail.com>
2022-01-20 22:40:17 +0900

    Add styled-components and theme provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>,
   document.getElementById("root")
 );
---
+++ src/styles/theme.js
@@ -0,0 +1,51 @@
+const fontSizes = {
+  large: "24px",
+  medium: "18px",
+  small: "16px",
+  extraSmall: "12px",
+};
+
+const paddings = {};
+
+const margins = {};
+
+const greyscale = {
+  titleActive: "#14142B",
+  body: "#D9DBE9",
+  label: "#6E7191",
+  placeholder: "#A0A3BD",
+  line: "#D9DBE9",
+  inputBackground: "#EFF0F6",
+  background: "#F7F7FC",
+  offWhite: "#FEFEFE",
+};
+
+const colors = {
+  blue: "#007AFF",
+  lightBlue: "#C7EBFF",
+  darkBlue: "#004DE3",
+  purple: "#0025E7",
+  lightPurple: "#CCD4FF",
+  darkPurple: "#020070",
+  red: "#FF3B30",
+  lightRed: "#FFD1CF",
+  darkRed: "#C60B00",
+  green: "#34C759",
+  lightGreen: "#DDFFE6",
+  darkGreen: "#00A028",
+};
+
+const theme = {
+  fontSizes,
+  greyscale,
+  colors,
+  deviceSizes,
+  device,
+  paddings,
+  margins,
+  interval,
+  verticalInterval,
+  gradient,
+};
+
+export default theme;

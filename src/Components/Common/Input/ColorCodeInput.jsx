2cb58ba944b53efad1240508eddaa432f05f2ea4
brad.ley <brad.ley@kakaocorp.com>
2022-01-24 19:41:07 +0900

    add component 'ColorCodeInput'

---
+++ src/Components/Common/Input/ColorCodeInput.jsx
@@ -0,0 +1,70 @@
+import React, { useCallback, useMemo, useState } from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import TextInput, { TEXT_INPUT_STATE, TEXT_INPUT_TYPE } from './TextInput'
+import { ReactComponent as RefreshIcon } from '../../../icon/ic-refresh.svg'
+import { COLOR } from '../../../style/commonStyle'
+
+const IconBox = styled.div`
+  display: inline-block;
+  cursor: pointer;
+
+  svg {
+    vertical-align: middle;
+    color: ${ COLOR.LABEL }
+  }
+`
+
+const ColorCodeInput = ({ onChangeListener }) => {
+  const [ inputValue, setInputValue ] = useState('#')
+  
+  const setInputValueWithListener = useCallback((value) => {
+    setInputValue(value)
+    
+    if (onChangeListener) {
+      onChangeListener(value)
+    }
+  }, [ onChangeListener ])
+  
+  const generateColorCode = useCallback(() => {
+    const colorValue = Math.floor((256 * 256 * 256 - 1) * Math.random())
+    const colorCode =
+      '#' + colorValue.toString(16)
+        .toUpperCase()
+        .padStart(6, '0')
+    
+    setInputValueWithListener(colorCode)
+  }, [])
+  
+  const onChange = useCallback((value, setValue) => {
+    if (!value.includes('#')) { // #이 지워지면 무시
+      setValue(inputValue)
+    } else if (value.match(/[^#a-fA-F0-9]/)) { // #, 0~9, a~f, A~F외의 문자가 입력되면 무시
+      setValue(inputValue)
+    } else if (value.length <= 7) { // 길이가 7이 넘지 않을 때 반영
+      setInputValueWithListener(value.toUpperCase())
+      setValue(value.toUpperCase())
+    } else { // 그 외의 경우 모두 무시
+      setValue(inputValue)
+    }
+  }, [ inputValue ])
+  
+  return (
+    <TextInput
+      type={ TEXT_INPUT_TYPE.SMALL }
+      state={ TEXT_INPUT_STATE.NORMAL }
+      placeholder="Label"
+      onChangeListener={ onChange }
+      inputValue={ inputValue }>
+      <IconBox onClick={ generateColorCode }>
+        <RefreshIcon
+          width="1em"
+          height="1em" />
+      </IconBox>
+    </TextInput>
+  )
+}
+
+ColorCodeInput.propTypes = {}
+
+export default ColorCodeInput

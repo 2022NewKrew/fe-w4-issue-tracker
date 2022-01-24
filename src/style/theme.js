645cab3642976f8ab205302d7d2925b0af78896b
Remian103 <alsdnska@naver.com>
2022-01-24 16:15:02 +0900

    feat: global style 적용
    reset.css 적용
    styled-components 의 theme 사용
    web font 사용

---
+++ src/style/theme.js
@@ -0,0 +1,34 @@
+export default {
+  grayscale: {
+    titleActive: "#14142B",
+    body: "#4E4B66",
+    label: "#6E7191",
+    placeholder: "#A0A3BD",
+    line: "#D9DBE9",
+    inputBackground: "#EFF0F6",
+    background: "#F7F7FC",
+    offWhite: "#FEFEFE",
+  },
+  color: {
+    primary: {
+      default: "#007AFF",
+      light: "#C7EBFF",
+      dark: "#004DE3",
+    },
+    secondary: {
+      default: "#0025E7",
+      light: "#CCD4FF",
+      dark: "#020070",
+    },
+    error: {
+      default: "#FF3B30",
+      light: "#FFD1CF",
+      dark: "#C60B00",
+    },
+    success: {
+      default: "#34C759",
+      light: "#DDFFE6",
+      dark: "#00A028",
+    },
+  },
+};

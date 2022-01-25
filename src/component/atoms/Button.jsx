7a78a97f884ea6d6c4effa76e71a8569a096ef59
Remian103 <alsdnska@naver.com>
2022-01-25 17:34:34 +0900

    refactor: Button 컴포넌트 1개로 통합
    optios prop 으로 버튼 타입을 선택할 수 있도록 구현

--- src/component/atoms/Button.jsx
+++ src/component/atoms/Button.jsx
@@ -2,130 +2,97 @@ import styled, { css, useTheme } from "styled-components";
 import { Icon } from "./Icons";
 import Text from "./Text";
 
-export const LargeButton = ({ color = "primary", prefixIcon, children, ...props }) => {
+export const Button = ({ options, children, ...props }) => {
   const theme = useTheme();
+  const { type, prefixIcon, color = "primary" } = options;
+
+  const buttonOption = { ...buttonType[type], color };
+  const textOption = buttonOption.textOption;
+
+  const prefixIconColor = {
+    filled: theme.grayscale.offWhite,
+    empty: theme.color[color].default,
+    text: theme.grayscale.label,
+  };
+
   return (
-    <FilledButton options={{ size: "large", color }} {...props}>
-      {prefixIcon && <Icon name={prefixIcon} color={theme.grayscale.offWhite} />}
-      <Text options={{ size: "medium", isLink: true }}>{children}</Text>
-    </FilledButton>
+    <StyledButton options={buttonOption} {...props}>
+      {prefixIcon && <Icon name={prefixIcon} color={prefixIconColor[buttonOption.style]} />}
+      <Text options={textOption}>{children}</Text>
+    </StyledButton>
   );
 };
 
-export const MediumStandardButton = ({ color = "primary", prefixIcon, children, ...props }) => {
-  const theme = useTheme();
-  return (
-    <FilledButton options={{ size: "medium", color }} {...props}>
-      {prefixIcon && <Icon name={prefixIcon} color={theme.grayscale.offWhite} />}
-      <Text options={{ size: "medium", isLink: true }}>{children}</Text>
-    </FilledButton>
-  );
-};
-
-export const SmallStandardButton = ({ color = "primary", prefixIcon, children, ...props }) => {
-  const theme = useTheme();
-  return (
-    <FilledButton options={{ size: "small", color }} {...props}>
-      {prefixIcon && <Icon name={prefixIcon} color={theme.grayscale.offWhite} />}
-      <Text options={{ size: "xsmall", isLink: true }}>{children}</Text>
-    </FilledButton>
-  );
-};
-
-export const SmallSecondaryButton = ({ color = "primary", prefixIcon, children, ...props }) => {
-  const theme = useTheme();
-  return (
-    <EmptyButton options={{ size: "small", color }} {...props}>
-      {prefixIcon && <Icon name={prefixIcon} color={themeColor(theme, color).default} />}
-      <Text options={{ size: "xsmall", isLink: true }}>{children}</Text>
-    </EmptyButton>
-  );
-};
-
-export const MediumTextButton = ({ prefixIcon, children, ...props }) => {
-  const theme = useTheme();
-  return (
-    <TextButton options={{ size: "text" }} {...props}>
-      {prefixIcon && <Icon name={prefixIcon} color={theme.grayscale.label} />}
-      <Text options={{ size: "small", isLink: true }}>{children}</Text>
-    </TextButton>
-  );
-};
-
-export const SmallTextButton = ({ prefixIcon, children, ...props }) => {
-  const theme = useTheme();
-  return (
-    <TextButton options={{ size: "text" }} {...props}>
-      {prefixIcon && <Icon name={prefixIcon} color={theme.grayscale.label} />}
-      <Text options={{ size: "xsmall", isLink: true }}>{children}</Text>
-    </TextButton>
-  );
-};
-
-const Button = styled.button`
+const StyledButton = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-
-  ${({ options: { size } }) => (size ? sizeType[size] : null)}
-
-  // 좀 더 좋은 방법 있으면 나중에 수정 예정..
-  & > * {
+  * {
+    // 좀 더 좋은 방법 있으면 나중에 수정 예정..
     margin: 0px 2px;
   }
-`;
-
-const FilledButton = styled(Button)`
-  color: ${({ theme }) => theme.grayscale.offWhite};
-  background-color: ${({ theme, options: { color } }) => themeColor(theme, color).default};
-
-  &:enabled:hover {
-    background-color: ${({ theme, options: { color } }) => themeColor(theme, color).dark};
-  }
-
-  &:enabled:focus {
-    box-shadow: 0 0 0 4px ${({ theme, options: { color } }) => themeColor(theme, color).light};
-  }
-
   &:disabled {
     opacity: 0.5;
   }
+
+  ${({ theme, options }) => {
+    return css`
+      ${sizeType[options.size]}
+      ${styleType({ theme, style: options.style, color: options.color })}
+    `;
+  }}
 `;
 
-const EmptyButton = styled(Button)`
-  color: ${({ theme, options: { color } }) => themeColor(theme, color).default};
-  background-color: ${({ theme }) => theme.grayscale.offWhite};
-  border: 2px solid ${({ theme, options: { color } }) => themeColor(theme, color).default};
-
-  &:enabled:hover {
-    color: ${({ theme, options: { color } }) => themeColor(theme, color).dark};
-    border-color: ${({ theme, options: { color } }) => themeColor(theme, color).dark};
-  }
-
-  &:enabled:focus {
-    box-shadow: 0 0 0 4px ${({ theme, options: { color } }) => themeColor(theme, color).light};
-  }
-
-  &:disabled {
-    opacity: 0.5;
-  }
-`;
-
-const TextButton = styled(Button)`
-  color: ${({ theme }) => theme.grayscale.label};
-
-  &:hover {
-    color: ${({ theme }) => theme.grayscale.body};
-  }
-
-  &:active {
-    color: ${({ theme }) => theme.grayscale.titleActive};
-  }
-
-  &:disabled {
-    opacity: 0.5;
-  }
-`;
+const buttonType = {
+  Large: {
+    size: "large",
+    style: "filled",
+    textOption: {
+      size: "medium",
+      isLink: true,
+    },
+  },
+  "Medium-Standard": {
+    size: "medium",
+    style: "filled",
+    textOption: {
+      size: "medium",
+      isLink: true,
+    },
+  },
+  "Small-Standard": {
+    size: "small",
+    style: "filled",
+    textOption: {
+      size: "xsmall",
+      isLink: true,
+    },
+  },
+  "Small-Secondary": {
+    size: "small",
+    style: "empty",
+    textOption: {
+      size: "xsmall",
+      isLink: true,
+    },
+  },
+  "Medium-Text": {
+    size: "text",
+    style: "text",
+    textOption: {
+      size: "small",
+      isLink: true,
+    },
+  },
+  "Small-Text": {
+    size: "text",
+    style: "text",
+    textOption: {
+      size: "xsmall",
+      isLink: true,
+    },
+  },
+};
 
 const sizeType = {
   large: css`
@@ -151,4 +118,44 @@ const sizeType = {
   `,
 };
 
-const themeColor = (theme, color) => theme.color[color] ?? { default: color, dark: color, light: color };
+const styleType = ({ theme, style, color }) => {
+  const { default: base, dark, light } = theme.color[color];
+  switch (style) {
+    case "filled":
+      return css`
+        color: ${theme.grayscale.offWhite};
+        background-color: ${base};
+        &:enabled:hover {
+          background-color: ${dark};
+        }
+        &:enabled:focus {
+          box-shadow: 0 0 0 4px ${light};
+        }
+      `;
+    case "empty":
+      return css`
+        color: ${base};
+        background-color: ${theme.grayscale.offWhite};
+        border: 2px solid ${base};
+        &:enabled:hover {
+          color: ${dark};
+          border-color: ${dark};
+        }
+        &:enabled:focus {
+          box-shadow: 0 0 0 4px ${light};
+        }
+      `;
+    case "text":
+      return css`
+        color: ${theme.grayscale.label};
+        &:hover {
+          color: ${theme.grayscale.body};
+        }
+        &:active {
+          color: ${theme.grayscale.titleActive};
+        }
+      `;
+    default:
+      throw Error("button style error");
+  }
+};

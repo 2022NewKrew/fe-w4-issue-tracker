584626b3eb0fbf3a8adf961b11c04df915312324
miro-ring <miro.ring@kakaocorp.com>
2022-01-26 22:35:11 +0900

    feat: /를 기준으로 문자열을 자르되, /를 포함하는 정규표현식 작성

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,5 @@ export const dispatchPopStateEvent = () => {
     const navigationEvent = new PopStateEvent('navigate');
     window.dispatchEvent(navigationEvent);
 };
+
+export const regSeparateBasedSlash = /(?=\/)/g;

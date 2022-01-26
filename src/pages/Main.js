4537f709f7c7dcea450ed77ecc64d0e0658d5787
Chae Lin Shin <chaelinshin96@gmail.com>
2022-01-26 18:47:01 +0900

    Add Dropdown Panel component

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,6 +4,7 @@ import { DropdownIndicators } from "@components/dropdownIndicators";
 import { FilterBar } from "@components/filterBar";
 import { Taps } from "@components/Taps";
 import { LargeLabel, SmallLabel } from "@components/Labels";
+import { DropdownPanel } from "@components/DropdownPanel";
 
 import { firebaseAuth } from "../firebase";
 
@@ -21,6 +22,14 @@ export default function Main() {
     setUser(null);
     removeCookie("user");
   }
+
+  const dropdownMenus = [
+    { id: 0, name: "열린 이슈" },
+    { id: 1, name: "내가 작성한 이슈" },
+    { id: 2, name: "나에게 할당된 이슈" },
+    { id: 3, name: "내가 댓글을 남긴 이슈" },
+    { id: 4, name: "닫힌 이슈" },
+  ];
   return (
     <div>
       Main Page <br />
@@ -34,6 +43,10 @@ export default function Main() {
       <SmallLabel type='dark' text='레이블 이름' />
       <SmallLabel type='light' text='레이블 이름' />
       <SmallLabel type='line' text='작성자' />
+      <br />
+      <DropdownPanel type='text' menus={dropdownMenus} header='필터 이름' />
+      <DropdownPanel type='image' menus={dropdownMenus} header='필터 이름' />
+      <DropdownPanel type='modify' menus={dropdownMenus} header='상태 변경' />
     </div>
   );
 }

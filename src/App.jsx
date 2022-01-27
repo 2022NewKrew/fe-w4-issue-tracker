2c83ce5c0209246c54598441113fe8664b6a8374
Remian103 <alsdnska@naver.com>
2022-01-27 14:17:09 +0900

    feat: issue page, test page 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
-import { Link, Navigate, Outlet, Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { Link, Navigate, Route, Routes } from "react-router-dom";
+import { useTheme } from "styled-components";
+import { IssueHome } from "./component/page/issue/IssueHome";
+import { IssuePage } from "./component/page/issue/IssuePage";
+import { TestPage } from "./component/page/test/TestPage";
+import { TestNew } from "./component/page/test/TestNew";
+import { Button } from "./component/atoms/Button";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Navigate to="/test" />} />
+        <Route path="/" element={<Navigate to="/issue" />} />
+        <Route path="/issue" element={<IssuePage />}>
+          <Route path="" element={<IssueHome />} />
         <Route
-          path="/test"
+            path="*"
           element={
-            <div>
-              <p>test</p>
-              <Link to="new">new-link</Link>
-              <Outlet />
-            </div>
+              <Link to="/issue">
+                <Button options={{ type: "Medium-Standard" }}>이슈 목록으로</Button>
+              </Link>
           }
-        >
-          <Route path="new" element={<div>new</div>} />
+          />
+        </Route>
+        <Route path="/test" element={<TestPage />}>
+          <Route path="new" element={<TestNew />} />
         </Route>
       </Routes>
     </>

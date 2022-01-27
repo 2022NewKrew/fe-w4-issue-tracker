6dd508fd943480c14ec663f465ba44de5c72fe88
miro-ring <miro.ring@kakaocorp.com>
2022-01-27 14:45:56 +0900

    refactor: 테스트 파일들 제거

--- src/components/User.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { useParams } from '@core/react-router-dom/useParams';
-
-export function User() {
-    const { id } = useParams();
-    return <div>User Num is {id}</div>;
-}

bd02246a306331394834746049d52527f61691ae
miro-ring <miro.ring@kakaocorp.com>
2022-02-01 18:33:38 +0900

    feat: 공통 컴포넌트(Button) 완성

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -8,8 +8,8 @@ const BasicButton = styled.button`
     ${(props) =>
         props.large &&
         css`
-            padding: 16px 24px;
             width: 340px;
+            padding: 16px 24px;
             &: focus {
                 border: 4px solid #c7ebff;
                 padding: 12px 24px;
@@ -29,6 +29,7 @@ const BasicButton = styled.button`
 
 const BasicDisabledButton = styled.button`
     ${({ theme }) => theme.basicButton}
+    cursor: default;
     opacity: 0.5;
     ${(props) =>
         props.large &&
@@ -44,15 +45,147 @@ const BasicDisabledButton = styled.button`
         `}
 `;
 
+const SmallButton = styled.button`
+    ${({ theme }) => theme.smallButton}
+    ${(props) =>
+        props.small_standard &&
+        css`
+            background-color: #007aff;
+            color: #fefefe;
+            &: hover {
+                background-color: #004de3;
+            }
+
+            &: focus {
+                padding: 6px 21px;
+                border: 4px solid #c7ebff;
+            }
+        `}
+    ${(props) =>
+        props.small_secondary &&
+        css`
+            background-color: #fff;
+            color: #007aff;
+            border: 2px solid #007aff;
+            padding: 8px 24px;
+            &: hover {
+                color: #004de3;
+                border-color: #004de3;
+            }
+
+            &: focus {
+                padding: 6px 22px;
+                border: 4px solid #c7ebff;
+            }
+        `}
+`;
+
+const SmallDisabledButton = styled.button`
+    ${({ theme }) => theme.smallButton}
+    cursor: default;
+    opacity: 0.5;
+    ${(props) =>
+        props.small_standard &&
+        css`
+            background-color: #007aff;
+            color: #fefefe;
+        `}
+    ${(props) =>
+        props.small_secondary &&
+        css`
+            background-color: #fff;
+            color: #007aff;
+            border: 2px solid #007aff;
+            padding: 8px 24px;
+        `}
+`;
+
+const TextButton = styled.button`
+    ${({ theme }) => theme.textButton}
+    &: active {
+        color: #14142b;
+    }
+    &: hover {
+        color: #4e4b66;
+    }
+    ${(props) =>
+        props.medium_text &&
+        css`
+            width: 87px;
+            font-size: 16px;
+            line-height: 32px;
+        `}
+    ${(props) =>
+        props.small_text &&
+        css`
+            width: 70px;
+            font-size: 12px;
+            line-height: 32px;
+        `}
+`;
+
+const TextDisabledButton = styled.button`
+    ${({ theme }) => theme.textButton}
+    cursor: default;
+    opacity: 0.5;
+    ${(props) =>
+        props.medium_text &&
+        css`
+            width: 87px;
+            font-size: 16px;
+            line-height: 32px;
+        `}
+    ${(props) =>
+        props.small_text &&
+        css`
+            width: 70px;
+            font-size: 12px;
+            line-height: 32px;
+        `}
+`;
 
 export function Button({ children, ...buttonProps }) {
-    if (buttonProps.large || buttonProps.medium_standard) {
-        return buttonProps.disabled ? (
+    const {
+        large,
+        medium_standard,
+        disabled,
+        small_standard,
+        small_secondary,
+        medium_text,
+        small_text,
+    } = buttonProps;
+
+    if ((large && disabled) || (medium_standard && disabled)) {
+        return (
             <BasicDisabledButton {...buttonProps}>
                 {children}
             </BasicDisabledButton>
-        ) : (
-            <BasicButton {...buttonProps}>{children}</BasicButton>
         );
+    }
+
+    if (large || medium_standard) {
+        return <BasicButton {...buttonProps}>{children}</BasicButton>;
+    }
+
+    if ((small_standard && disabled) || (small_secondary && disabled)) {
+        return (
+            <SmallDisabledButton {...buttonProps}>
+                {children}
+            </SmallDisabledButton>
+        );
+    }
+
+    if (small_standard || small_secondary) {
+        return <SmallButton {...buttonProps}>{children}</SmallButton>;
+    }
+
+    if ((medium_text && disabled) || (small_text && disabled)) {
+        return (
+            <TextDisabledButton {...buttonProps}>{children}</TextDisabledButton>
+        );
+    }
+
+    if (medium_text || small_text) {
+        return <TextButton {...buttonProps}>{children}</TextButton>;
     }
 }

9ed3624cde4b821b305299ada3d550ddf39bde86
miro-ring <miro.ring@kakaocorp.com>
2022-02-04 19:30:14 +0900

    feat: 공통 컴포넌트(버튼) 구현 완료

--- client/src/components/common/Button.jsx
+++ client/src/components/common/Button.jsx
@@ -45,6 +45,113 @@ const BasicDisabledButton = styled.button`
         `}
 `;
 
+const SmallButton = styled.button`
+    ${({ theme }) => theme.smallButton}
+    ${(props) =>
+        props.small_standard &&
+        css`
+            background-color: #007aff;
+            color: #fefefe;
+            &: hover {
+                background-color: #004de3;
+            }
+
+            &: focus {
+                padding: 6px 21px;
+                border: 4px solid #c7ebff;
+            }
+        `}
+    ${(props) =>
+        props.small_secondary &&
+        css`
+            background-color: #fff;
+            color: #007aff;
+            border: 2px solid #007aff;
+            padding: 8px 24px;
+            &: hover {
+                color: #004de3;
+                border-color: #004de3;
+            }
+
+            &: focus {
+                padding: 6px 22px;
+                border: 4px solid #c7ebff;
+            }
+        `}
+`;
+
+const SmallDisabledButton = styled.button`
+    ${({ theme }) => theme.smallButton}
+    cursor: default;
+    opacity: 0.5;
+    ${(props) =>
+        props.small_standard &&
+        css`
+            background-color: #007aff;
+            color: #fefefe;
+        `}
+    ${(props) =>
+        props.small_secondary &&
+        css`
+            background-color: #fff;
+            color: #007aff;
+            border: 2px solid #007aff;
+            padding: 8px 24px;
+        `}
+`;
+
+const TextButton = styled.button`
+    ${({ theme }) => theme.textButton}
+    &: active {
+        color: #14142b;
+    }
+    &: hover {
+        color: #4e4b66;
+    }
+    ${(props) =>
+        props.medium_text &&
+        css`
+            width: 87px;
+            font-size: 16px;
+            line-height: 32px;
+        `}
+    ${(props) =>
+        props.small_text &&
+        css`
+            width: 70px;
+            font-size: 12px;
+            line-height: 32px;
+        `}
+`;
+
+const TextDisabledButton = styled.button`
+    ${({ theme }) => theme.textButton}
+    cursor: default;
+    opacity: 0.5;
+    ${(props) =>
+        props.medium_text &&
+        css`
+            width: 87px;
+            font-size: 16px;
+            line-height: 32px;
+        `}
+    ${(props) =>
+        props.small_text &&
+        css`
+            width: 70px;
+            font-size: 12px;
+            line-height: 32px;
+        `}
+`;
+
+const GithubLoginButton = styled.button`
+    ${({ theme }) => theme.basicButton}
+    margin-top: 61px;
+    width: 340px;
+    padding: 16px 24px;
+    background-color: #14142b;
+`;
+
 export function Button({ children, ...buttonProps }) {
     const {
         large,
@@ -54,6 +161,7 @@ export function Button({ children, ...buttonProps }) {
         small_secondary,
         medium_text,
         small_text,
+        github,
     } = buttonProps;
 
     if ((large && disabled) || (medium_standard && disabled)) {
@@ -68,4 +176,29 @@ export function Button({ children, ...buttonProps }) {
         return <BasicButton {...buttonProps}>{children}</BasicButton>;
     }
 
+    if ((small_standard && disabled) || (small_secondary && disabled)) {
+        return (
+            <SmallDisabledButton {...buttonProps}>
+                {children}
+            </SmallDisabledButton>
+        );
+    }
+
+    if (small_standard || small_secondary) {
+        return <SmallButton {...buttonProps}>{children}</SmallButton>;
+    }
+
+    if ((medium_text && disabled) || (small_text && disabled)) {
+        return (
+            <TextDisabledButton {...buttonProps}>{children}</TextDisabledButton>
+        );
+    }
+
+    if (medium_text || small_text) {
+        return <TextButton {...buttonProps}>{children}</TextButton>;
+    }
+
+    if (github) {
+        return <GithubLoginButton>{children}</GithubLoginButton>;
+    }
 }

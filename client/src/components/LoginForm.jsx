4dbdbed364b4d060cd6b0ee0b880afd3636c39a6
miro-ring <miro.ring@kakaocorp.com>
2022-02-08 22:53:52 +0900

    refactor: form 영역 분리

---
+++ client/src/components/LoginForm.jsx
@@ -0,0 +1,109 @@
+import { useState } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { login, getUserInfo } from '@api';
+import { useNavigate } from '@core/react-router-dom/useNavigate';
+import { Button, Input } from '@components';
+import { userInfoSelector } from '@states';
+import { setLocalStorage } from '@utils';
+
+export function LoginForm() {
+    const [userId, setUserId] = useState('');
+    const [userIdType, setUserIdType] = useState(false);
+    const [password, setPassword] = useState('');
+    const [passwordType, setPasswordType] = useState(false);
+
+    const setUserInfo = useSetRecoilState(userInfoSelector);
+
+    const chkUserIdAvailable = userId.length >= 6 && userId.length <= 16;
+    const chkPasswordAvailable = password.length >= 6 && password.length <= 12;
+
+    const navigate = useNavigate();
+
+    const handleInputUserId = ({ target: { value } }) => {
+        setUserId(value);
+
+        if (value.length === 0) {
+            setUserIdType(false);
+            return;
+        }
+        setUserIdType(true);
+    };
+
+    const handleInputPassword = ({ target: { value } }) => {
+        setPassword(value);
+
+        if (value.length === 0) {
+            setPasswordType(false);
+            return;
+        }
+        setPasswordType(true);
+    };
+
+    const handleSubmit = async (e) => {
+        try {
+            e.preventDefault();
+
+            if (!chkUserIdAvailable) {
+                throw new Error(
+                    '아이디는 최소 6자리에서 16자리까지 입력할 수 있습니다.'
+                );
+            }
+
+            if (!chkPasswordAvailable) {
+                throw new Error(
+                    '비밀번호는 최소 6자리에서 12자리까지 입력할 수 있습니다.'
+                );
+            }
+
+            const {
+                data: { loginSuccess, message, token },
+            } = await login({
+                email: userId,
+                password: password,
+            });
+
+            if (!loginSuccess) {
+                throw new Error(message);
+            }
+            setLocalStorage('token', token);
+
+            const { data } = await getUserInfo();
+            setUserInfo({ ...data });
+            navigate('/issue');
+        } catch (e) {
+            alert(e.message);
+            console.error(e.message);
+        }
+    };
+
+    const chkButtonCanClick = () => {
+        if (chkUserIdAvailable && chkPasswordAvailable) {
+            return true;
+        }
+        return false;
+    };
+
+    return (
+        <form action="#" method="post" onSubmit={handleSubmit}>
+            <Input
+                type="text"
+                required
+                handleChange={handleInputUserId}
+                className={userIdType && 'type'}
+                large
+                label="아이디"
+            />
+            <Input
+                type="password"
+                required
+                handleChange={handleInputPassword}
+                className={passwordType && 'type'}
+                large
+                label="비밀번호"
+            />
+            <Button type="submit" large disabled={!chkButtonCanClick()}>
+                아이디로 로그인
+            </Button>
+        </form>
+    );
+}
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,5 @@
-import { useState } from 'react';
-import { useRecoilState } from 'recoil';
 import styled from 'styled-components';
-import { login, getUserInfo } from '@api';
-import { useNavigate } from '@core/react-router-dom/useNavigate';
-import { Button, Input, Logo } from '@components';
-import { setLocalStorage } from '@utils';
-import { userInfoSelector } from '../states';
+import { Button, Logo, LoginForm } from '@components';
 
 const Wrapper = styled.div`
     margin: 0 auto;
@@ -37,107 +31,12 @@ const Register = styled.button`
 `;
 
 export function Login() {
-    const [userId, setUserId] = useState('');
-    const [userIdType, setUserIdType] = useState(false);
-    const [password, setPassword] = useState('');
-    const [passwordType, setPasswordType] = useState(false);
-
-    const [userInfo, setUserInfo] = useRecoilState(userInfoSelector);
-
-    const chkUserIdAvailable = userId.length >= 6 && userId.length <= 16;
-    const chkPasswordAvailable = password.length >= 6 && password.length <= 12;
-
-    const navigate = useNavigate();
-
-    const handleInputUserId = ({ target: { value } }) => {
-        setUserId(value);
-
-        if (value.length === 0) {
-            setUserIdType(false);
-            return;
-        }
-        setUserIdType(true);
-    };
-
-    const handleInputPassword = ({ target: { value } }) => {
-        setPassword(value);
-
-        if (value.length === 0) {
-            setPasswordType(false);
-            return;
-        }
-        setPasswordType(true);
-    };
-
-    const handleSubmit = async (e) => {
-        try {
-            e.preventDefault();
-
-            if (!chkUserIdAvailable) {
-                throw new Error(
-                    '아이디는 최소 6자리에서 16자리까지 입력할 수 있습니다.'
-                );
-            }
-
-            if (!chkPasswordAvailable) {
-                throw new Error(
-                    '비밀번호는 최소 6자리에서 12자리까지 입력할 수 있습니다.'
-                );
-            }
-            const {
-                data: { loginSuccess, message, token },
-            } = await login({
-                email: userId,
-                password: password,
-            });
-            if (!loginSuccess) {
-                throw new Error(message);
-            }
-            setLocalStorage('token', token);
-            navigate('/issue');
-        } catch (e) {
-            alert(e.message);
-            console.error(e.message);
-        }
-    };
-
-    const chkButtonCanClick = () => {
-        if (chkUserIdAvailable && chkPasswordAvailable) {
-            return true;
-        }
-        return false;
-    };
-
     return (
         <Wrapper>
             <Logo large />
             <Button github>Github 계정으로 로그인</Button>
             <Separator>or</Separator>
-            <form action="#" method="post" onSubmit={handleSubmit}>
-                <Input
-                    type="text"
-                    required
-                    handleChange={handleInputUserId}
-                    className={userIdType && 'type'}
-                    large
-                    label="아이디"
-                />
-                <Input
-                    type="password"
-                    required
-                    handleChange={handleInputPassword}
-                    className={passwordType && 'type'}
-                    large
-                    label="비밀번호"
-                />
-                <Button
-                    type="submit"
-                    large
-                    disabled={chkButtonCanClick() ? false : true}
-                >
-                    아이디로 로그인
-                </Button>
-            </form>
+            <LoginForm />
             <Register>회원가입</Register>
         </Wrapper>
     );

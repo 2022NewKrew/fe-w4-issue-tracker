336084f19e4be3bdf7dd4a7eae89669da1cec12b
miro-ring <miro.ring@kakaocorp.com>
2022-02-08 14:43:53 +0900

    feat: storage와 관련된 유틸 함수들 추가

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -4,3 +4,11 @@ export const dispatchPopStateEvent = () => {
 };
 
 export const regSeparateBasedSlash = /(?=\/)/g;
+
+export const setLocalStorage = (key, value) => {
+    localStorage.setItem(key, JSON.stringify(value));
+};
+
+export const getLocalStorage = (key) => {
+    return JSON.parse(localStorage.getItem(key));
+};

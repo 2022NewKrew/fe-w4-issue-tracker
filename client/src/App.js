52ffdd889860369303cc8b1cb26e50d4168f93d1
miro-ring <miro.ring@kakaocorp.com>
2022-02-08 14:43:33 +0900

    feat: recoil을 활용해 사용자 정보 전역 데이터로 관리하기 위한 설정 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
 import { ThemeProvider } from 'styled-components';
+import { RecoilRoot } from 'recoil';
 import { Routes, Route } from '@core/react-router-dom';
-import { NotFound } from '@components';
+import { Login, IssueList, NotFound } from '@pages';
 import { GlobalStyle, theme } from '@style';
+import React from 'react';
 
 export default function App() {
     return (
         <>
             <ThemeProvider theme={theme}>
                 <GlobalStyle />
-                <Routes>
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
+                <RecoilRoot>
+                    <React.Suspense fallback={<div>Loading</div>}>
+                        <Routes>
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/issue" element={<IssueList />} />
+                            <Route path="*" element={<NotFound />} />
+                        </Routes>
+                    </React.Suspense>
+                </RecoilRoot>
             </ThemeProvider>
         </>
     );

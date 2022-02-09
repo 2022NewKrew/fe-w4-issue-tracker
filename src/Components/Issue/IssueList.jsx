d1cf6f0d441929dff0b368ede1d33140c5ddf4e6
brad.ley <brad.ley@kakaocorp.com>
2022-02-09 13:13:54 +0900

    IssueList의 css style 및 템플릿 수정

--- src/Components/Issue/IssueList.jsx
+++ src/Components/Issue/IssueList.jsx
@@ -17,8 +17,23 @@ const Header = styled.div`
   height: 64px;
   display: flex;
   justify-content: space-between;
-  background-color: ${COLOR.LINE};
+  background-color: ${COLOR.BACKGROUND};
   border-radius: 16px 16px 0 0;
+`
+
+const HorizontalDiv = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const SmallDropdownWrapper = styled.div`
+  width: 70px;
+  margin-right: 30px;
+`
+
+const LargeDropdownWrapper = styled.div`
+  width: 84px;
+  margin-right: 30px;
 `
 
 const List = styled.ul`
@@ -29,61 +44,74 @@ const ItemWrapper = styled.li`
   width: 100%;
   height: 100px;
   border-top: 1px solid ${COLOR.LINE};
+
+  &:last-child {
+    border-radius: 0 0 16px 16px;
+    overflow: hidden;
+  }
 `
 
 const IssueList = ({ issueList }) => {
   return (
     <Container>
       <Header>
-        <div>
+        <HorizontalDiv>
           <span>열린 이슈</span>
           <span>닫힌 이슈</span>
-        </div>
-        <div>
-          <Dropdown
-            indicatorText="담당자"
-            panelTitle="담당자"
-            type={DROPDOWN_ITEM_TYPE.RADIO_BTN}
-            itemInfoList={[
-              { text: '담당자가 없는 이슈' },
-              {
-                text: 'Oni',
-                imgSrc:
-                  'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
-              },
-            ]}
-          />
-          <Dropdown
-            indicatorText="레이블"
-            panelTitle="레이블"
-            type={DROPDOWN_ITEM_TYPE.RADIO_BTN}
-            itemInfoList={[
-              { text: '레이블이 없는 이슈' },
-              { text: 'documentation' },
-            ]}
-          />
-          <Dropdown
-            indicatorText="마일스톤"
-            panelTitle="마일스톤"
-            type={DROPDOWN_ITEM_TYPE.RADIO_BTN}
-            itemInfoList={[
-              { text: '마일스톤이 없는 이슈' },
-              { text: '마스터즈 코스' },
-            ]}
-          />
-          <Dropdown
-            indicatorText="작성자"
-            panelTitle="작성자"
-            type={DROPDOWN_ITEM_TYPE.RADIO_BTN}
-            itemInfoList={[
-              {
-                text: 'Oni',
-                imgSrc:
-                  'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
-              },
-            ]}
-          />
-        </div>
+        </HorizontalDiv>
+        <HorizontalDiv>
+          <SmallDropdownWrapper>
+            <Dropdown
+              indicatorText="담당자"
+              panelTitle="담당자"
+              type={DROPDOWN_ITEM_TYPE.RADIO_BTN}
+              itemInfoList={[
+                { text: '담당자가 없는 이슈' },
+                {
+                  text: 'Oni',
+                  imgSrc:
+                    'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
+                },
+              ]}
+            />
+          </SmallDropdownWrapper>
+          <SmallDropdownWrapper>
+            <Dropdown
+              indicatorText="레이블"
+              panelTitle="레이블"
+              type={DROPDOWN_ITEM_TYPE.RADIO_BTN}
+              itemInfoList={[
+                { text: '레이블이 없는 이슈' },
+                { text: 'documentation' },
+              ]}
+            />
+          </SmallDropdownWrapper>
+          <LargeDropdownWrapper>
+            <Dropdown
+              indicatorText="마일스톤"
+              panelTitle="마일스톤"
+              type={DROPDOWN_ITEM_TYPE.RADIO_BTN}
+              itemInfoList={[
+                { text: '마일스톤이 없는 이슈' },
+                { text: '마스터즈 코스' },
+              ]}
+            />
+          </LargeDropdownWrapper>
+          <SmallDropdownWrapper>
+            <Dropdown
+              indicatorText="작성자"
+              panelTitle="작성자"
+              type={DROPDOWN_ITEM_TYPE.RADIO_BTN}
+              itemInfoList={[
+                {
+                  text: 'Oni',
+                  imgSrc:
+                    'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
+                },
+              ]}
+            />
+          </SmallDropdownWrapper>
+        </HorizontalDiv>
       </Header>
       <List>
         {issueList.map((issue) => (

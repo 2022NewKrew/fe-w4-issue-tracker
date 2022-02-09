a05f0eaa7ef7b6a249ee80fd84c3795f42cfde50
miro-ring <miro.ring@kakaocorp.com>
2022-02-09 20:11:43 +0900

    feat: 전체 너비 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
-import { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import { RecoilRoot } from 'recoil';
 import { Routes, Route } from '@core/react-router-dom';
 import { Login, IssueList, NotFound } from '@pages';
 import { GlobalStyle, theme } from '@style';
 import React from 'react';
+
+const Wrapper = styled.div`
+    max-width: 1440px;
+    margin: 0 auto;
+`;
 
 export default function App() {
     return (
@@ -12,11 +17,13 @@ export default function App() {
                 <GlobalStyle />
                 <RecoilRoot>
                     <React.Suspense fallback={<div>Loading</div>}>
-                        <Routes>
-                            <Route path="/login" element={<Login />} />
-                            <Route path="/issue" element={<IssueList />} />
-                            <Route path="*" element={<NotFound />} />
-                        </Routes>
+                        <Wrapper>
+                            <Routes>
+                                <Route path="/login" element={<Login />} />
+                                <Route path="/issue" element={<IssueList />} />
+                                <Route path="*" element={<NotFound />} />
+                            </Routes>
+                        </Wrapper>
                     </React.Suspense>
                 </RecoilRoot>
             </ThemeProvider>

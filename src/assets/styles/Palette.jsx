0a1c8adf41df3e565cc897ad52988d5d6bbd6ac4
dth <tlsdh1111@gmail.com>
2022-02-09 15:58:26 +0900

    chore : Palette 잘못된 값 변경

--- src/assets/styles/Palette.jsx
+++ src/assets/styles/Palette.jsx
@@ -5,7 +5,7 @@ export const labelGS = '#6E7191';
 export const placeHolderGS = '#A0A3BD';
 export const lineGS = '#D9DBE9';
 export const inputBackgroundGS = '#EFF0F6';
-export const backgroundGS = '#D9DBE9';
+export const backgroundGS = '#F7F7FC';
 export const offWhiteGS = '#FEFEFE';
 
 // Colors

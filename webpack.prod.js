bc3ba46fbac95fa4a4d3670d6215a07a3f34ffbc
dlguswo333 <trey.lee@kakaocorp.com>
2022-02-10 13:44:22 +0900

    Introduce webpack-bundle-analyzer

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,16 @@
 const {merge}=require('webpack-merge');
 const commonConfig=require('./webpack.common');
+const BundleAnalyzerPlugin=require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports=merge(commonConfig, {
-  mode: 'production'
+  mode: 'production',
+  plugins: [
+    new BundleAnalyzerPlugin({
+      generateStatsFile: true,
+      statsFilename: 'stats.json',
+      openAnalyzer: false,
+      analyzerMode: 'json'
+    })
+  ],
+  devtool: 'source-map'
 });

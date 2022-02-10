c713df696f633329699285e1f819c22218ac0ac5
miro-ring <miro.ring@kakaocorp.com>
2022-02-10 13:10:39 +0900

    refactor: 헤더의 불필요한 부분 제거

--- client/src/api/theAxios.js
+++ client/src/api/theAxios.js
@@ -6,10 +6,6 @@ export const SERVER_URL = 'http://localhost:5000';
 const theAxios = ({ url, type = 'get', param }) => {
     const headers = {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-        'Access-Control-Allow-Headers':
-            'Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization',
     };
 
     const accessToken = getLocalStorage('token');

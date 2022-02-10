ff3d3eae05f6e712a89cbc2bf48d22385bf79baf
ethan-3160 <ethan.3160@kakaocorp.com>
2022-02-10 09:10:18 +0900

    Feat : add CustomButton

--- packages/client/src/components/UI/Molecules/CustomButton.tsx
+++ packages/client/src/components/UI/Molecules/CustomButton.tsx
@@ -1,16 +1,44 @@
+import { useIssueFormStore, useIssueStore } from "@stores/issue";
 import { useLabelFormStore } from "@stores/label";
 import Atoms from "@UI/Atoms";
+import { useNavigate } from "react-router-dom";
+
+export const EditButton = ({ onClick }: { onClick?: any }) => (
+  <Atoms.Button
+    className="EditButton"
+    size="small"
+    shape="text"
+    icon="edit"
+    text="편집"
+    onClick={onClick}
+  />
+);
+
+export const RemoveButton = ({ onClick }: { onClick?: any }) => (
+  <Atoms.Button
+    className="RemoveButton"
+    size="small"
+    shape="text"
+    icon="trash_red"
+    text="삭제"
+    onClick={onClick}
+  />
+);
 
 export const LabelAddButton = () => {
-  const { labelFormMode, setLabelFormMode } = useLabelFormStore();
+  const { labelFormMode, setLabelFormMode, resetLabelForm } =
+    useLabelFormStore();
 
-  return labelFormMode === "close" ? (
+  return labelFormMode !== "add" ? (
     <Atoms.Button
       className="LabelAddButton"
       size="small"
       text="추가"
       icon="plus_white"
-      onClick={() => setLabelFormMode("add")}
+      onClick={() => {
+        resetLabelForm();
+        setLabelFormMode("add");
+      }}
     />
   ) : (
     <Atoms.Button
@@ -24,17 +52,128 @@ export const LabelAddButton = () => {
   );
 };
 
-export const LabelFormButton = () => {
-  const { labelForm, labelFormMode } = useLabelFormStore();
+interface FormButtonProps {
+  formMode: "add" | string;
+  onCancel: () => void;
+  disabled: boolean;
+}
 
-  return labelFormMode === "add" ? (
+export const FormButton = ({
+  onCancel,
+  formMode,
+  disabled,
+}: FormButtonProps) => {
+  return formMode === "add" ? (
     <Atoms.Button
-      className="LabelFormButton"
+      className="FormButton"
       size="small"
       icon="plus_white"
       text="완료"
       type="submit"
-      disabled={!labelForm.name}
+      disabled={disabled}
     />
-  ) : null;
+  ) : (
+    <Atoms.ButtonGroup gap={8}>
+      <Atoms.Button
+        className="FormButton"
+        shape="secondary"
+        size="small"
+        icon="x_square"
+        text="취소"
+        onClick={onCancel}
+      />
+      <Atoms.Button
+        className="FormButton"
+        size="small"
+        icon="edit"
+        text="완료"
+        type="submit"
+      />
+    </Atoms.ButtonGroup>
+  );
+};
+
+export const AddIssueButton = () => {
+  const nav = useNavigate();
+  const { resetIssueForm } = useIssueFormStore();
+
+  return (
+    <Atoms.Button
+      size="small"
+      icon="plus_white"
+      text="이슈 작성"
+      onClick={() => {
+        resetIssueForm();
+        nav("add");
+      }}
+    />
+  );
+};
+
+export const IssueFormButton = () => {
+  const { issueForm, resetIssueForm } = useIssueFormStore();
+  const nav = useNavigate();
+
+  return (
+    <Atoms.ButtonGroup gap={32}>
+      <Atoms.Button
+        size="medium"
+        shape="text"
+        icon="x_square"
+        text="작성 취소"
+        onClick={() => {
+          resetIssueForm();
+          nav("/issue");
+        }}
+      />
+      <Atoms.Button
+        size="medium"
+        type="submit"
+        disabled={!(issueForm.title && issueForm.comment)}
+        text="완료"
+      />
+    </Atoms.ButtonGroup>
+  );
+};
+
+export const IssueDetailButton = () => {
+  const { issueFormMode, issueTitleModify, setIssueTitleModify, setIssueForm } =
+    useIssueFormStore();
+  const { issueList } = useIssueStore();
+
+  const cancelEdit = () => {
+    const prevTitle = issueList.find(({ id }) => id === issueFormMode)
+      ?.title as string;
+    setIssueForm((prev) => ({ ...prev, title: prevTitle }));
+    setIssueTitleModify(false);
+  };
+
+  return issueTitleModify ? (
+    <Atoms.ButtonGroup gap={8}>
+      <Atoms.Button
+        size="small"
+        shape="secondary"
+        icon="x_square"
+        text="편집 취소"
+        onClick={cancelEdit}
+      />
+      <Atoms.Button size="small" icon="edit" text="편집 완료" />
+    </Atoms.ButtonGroup>
+  ) : (
+    <Atoms.ButtonGroup gap={8}>
+      <Atoms.Button
+        size="small"
+        shape="secondary"
+        icon="edit"
+        text="제목 편집"
+        onClick={() => setIssueTitleModify(true)}
+      />
+      <Atoms.Button
+        size="small"
+        shape="secondary"
+        icon="close"
+        text="이슈 닫기"
+      />
+    </Atoms.ButtonGroup>
+  );
 };
